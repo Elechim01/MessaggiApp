79b78eac57cf39c0162a4984b8198abce394f77d
Elechim01 <mikymanny015@gmail.com>
2021-01-02 10:34:29 +0100

    Initial Commit

---
+++ MessaggiApp/MessaggiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessaggiAppApp.swift
+//  MessaggiApp
+//
+//  Created by Michele on 02/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MessaggiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
